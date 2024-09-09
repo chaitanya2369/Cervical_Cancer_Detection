e3d630e638a4f4dd7373488d7a9623c2b68eaa4e
Sanmaireddy <ysreddy377@gmail.com>
2024-09-09 09:30:57 +0530

    Added Routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AdminHome from "./pages/admin/AdminHome";
+import UserHome from "./pages/user/UserHome";
+import TrainerHome from "./pages/trainer/TrainerHome";
+import Patients from "./pages/user/Patients";
+import ViewPatient from "./pages/user/ViewPatient";
+import ViewUsers from "./pages/admin/ViewUsers";
+
+const getRole = () => {
+  return "admin";
+};
+
+const getPermission = () => {
+  //1->has permission to view patient, 2-> send pics for training
+  return 1;
+};
+
+const renderHome = () => {
+  const role = getRole();
+  if (role == "admin") {
+    return <AdminHome />;
+  } else if (role == "user") {
+    return <UserHome />;
+  } else if (role == "trainer") {
+    return <TrainerHome />;
+  } else return <Login />;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const permission = getPermission();
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+    {
+      path: "/home",
+      element: renderHome(),
+    },
+    {
+      path: "/patients",
+      element: <Patients />,
+    },
+    {
+      path: "/patient/:slug",
+      element: <ViewPatient />,
+    },
+    {
+      path: "/users",
+      element: <ViewUsers />,
+    },
+  ]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
