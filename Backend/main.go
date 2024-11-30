eb63fb2ee838a32fdb886515244193c000cd7c6f
Sanmaireddy <ysreddy377@gmail.com>
2024-11-30 15:01:24 +0530

    Backend basic

---
+++ Backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"Cervical_Cancer_Detection/auth"
+	"Cervical_Cancer_Detection/db"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+
+func main() {
+	r := gin.Default()
+    r.Use(cors.Default())
+	
+	db.ConnectDb() //connect db
+
+	r.POST("/signup", auth.SignUp)
+
+	r.Run(":8080")	
+}
